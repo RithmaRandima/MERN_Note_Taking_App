22a2f596eb12378f6cf97a7bcd677e58e03f068f
RithmaRandima <rithma.r.nandasiri.com>
2025-07-17 22:27:08 +0530

    completed NPM Packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailsPage from "./pages/NoteDetailsPage";
+import toast from "react-hot-toast";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div data-theme="forest">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailsPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
